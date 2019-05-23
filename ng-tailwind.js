e60d2e9ee779d99c2d75d1ca40880dbd4dcce571
Jason Michaels <jason.michaels721@gmail.com>
2019-05-23 15:42:34 +0000

    tailwind css

---
+++ ng-tailwind.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: '/workspace/ng-seed/tailwind.config.js',
+  sourceCSS: '/workspace/ng-seed/src/tailwind.css',
+  outputCSS: '/workspace/ng-seed/src/styles.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: [
+    '.ts',
+    '.html',
+    '.js'
+  ]
+}
