0ddce5d87541280c1d74d91283bf6ae89b6a4abb
Jason Michaels <jason.michaels721@gmail.com>
2019-05-24 21:39:54 +0000

    storybook knobs

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -1,10 +1,17 @@
 import { storiesOf } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
 
 import { Welcome, Button } from '@storybook/angular/demo';
 
 import { AppComponent } from '../app/app.component';
+
+import { SimpleKnobsComponent } from '../quick-stories/knobs.component';
+
+const stories = storiesOf('Storybook Knobs', module);
+
+stories.addDecorator(withKnobs);
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
   component: Welcome,
@@ -16,6 +23,39 @@ storiesOf('AppComponent', module).add('Varaint 1', () => ({
   props: {},
 }));
 
+stories.add('Config a button', () => ({
+  component: Button,
+  props: {
+   text: text('text', 'Hello Storybook'), // The first param of the knob function has to be exactly the same as the component input.
+  },
+}));
+
+stories.add('Simple', () => {
+    const name = text('name', 'John Doe');
+    const age = number('age', 0);
+    const phoneNumber = text('phoneNumber', '555-55-55');
+
+    return {
+      moduleMetadata: {
+        entryComponents: [SimpleKnobsComponent],
+        declarations: [SimpleKnobsComponent],
+      },
+      template: `
+        <h1> This is a template </h1>
+        <storybook-simple-knobs-component
+          [age]="age"
+          [phoneNumber]="phoneNumber"
+          [name]="name"
+        >
+        </storybook-simple-knobs-component>
+      `,
+      props: {
+        name,
+        age,
+        phoneNumber,
+      },
+    };
+});
 
 storiesOf('Button', module)
   .add('with text', () => ({
